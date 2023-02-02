0668109fe91d4653baac13a19d549bb8fce185cd
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-02 05:58:39 +0300

    hero finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div>
-
+    <div className='max-w-[1640px] mx-auto p-4'>
+      <div className='max-h-[500px] relative'>
+        <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
+          <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span
+            className='text-orange-500'>Best</span></h1>
+          <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span
+            className='text-orange-500'>Foods </span>Delivered</h1>
+        </div>
+        <img
+          className='w-full max-h-[500px] object-cover'
+          src="https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80"
+          alt="/"/>
+      </div>
     </div>
   )
 }
