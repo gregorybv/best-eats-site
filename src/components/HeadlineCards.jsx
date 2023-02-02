d86192e412bbbede39a6da5b90d078721b84c93e
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-02 06:14:31 +0300

    headlinecards block finish

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -2,8 +2,40 @@ import React from 'react';
 
 const HeadlineCards = () => {
   return (
-    <div>
-
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+      <div className='rounded-xl relative'>
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+          <p className='px-2'>Through 8/26</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img
+          className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src="https://images.unsplash.com/photo-1551782450-17144efb9c50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80"
+          alt="/"/>
+      </div>
+      <div className='rounded-xl relative'>
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>New Restaurants</p>
+          <p className='px-2'>Added Daily</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img
+          className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src="https://images.unsplash.com/photo-1516511932045-31ec59a7c8d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          alt="/"/>
+      </div>
+      <div className='rounded-xl relative'>
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>We Deliver Deserts Too</p>
+          <p className='px-2'>Tasty Treats</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img
+          className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src="https://images.unsplash.com/photo-1524182732116-a3ad2034503c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+          alt="/"/>
+      </div>
     </div>
   )
 }
