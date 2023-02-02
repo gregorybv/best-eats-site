83d34bc01119448c63f8cbdd4e7eee6de509c711
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-02 07:15:19 +0300

    food block finish

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -5,6 +5,22 @@ const Food = () => {
 
   const [foods, setFoods] = useState(data)
 
+  const filterType = (category) => {
+    setFoods(
+      data.filter((item) => {
+        return item.category === category
+      })
+    )
+  }
+
+  const filterPrice = (price) => {
+    setFoods(
+      data.filter((item) => {
+        return item.price === price
+      })
+    )
+  }
+
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12'>
       <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
@@ -13,19 +29,24 @@ const Food = () => {
         <div>
           <p className='font-bold text-gray-700'>Filter Type</p>
           <div className='flex justify-between flex-wrap'>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button onClick={() => setFoods(data)}
+                    className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
               All
             </button>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button onClick={() => filterType('burger')}
+                    className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
               Burgers
             </button>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button onClick={() => filterType('pizza')}
+                    className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
               Pizza
             </button>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button onClick={() => filterType('salad')}
+                    className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
               Salads
             </button>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button onClick={() => filterType('chicken')}
+                    className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
               Chicken
             </button>
           </div>
@@ -33,10 +54,18 @@ const Food = () => {
         <div>
           <p className='font-bold text-gray-700'>Filter Price</p>
           <div className='flex justify-between max-w-[390px] w-full'>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$</button>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$</button>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$</button>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$</button>
+            <button onClick={() => filterPrice('$')}
+                    className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$
+            </button>
+            <button onClick={() => filterPrice('$$')}
+                    className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$
+            </button>
+            <button onClick={() => filterPrice('$$$')}
+                    className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$
+            </button>
+            <button onClick={() => filterPrice('$$$$')}
+                    className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$
+            </button>
           </div>
         </div>
       </div>
@@ -45,7 +74,7 @@ const Food = () => {
         {foods.map((item, index) => (
           <div key={index} className='border shadow-lg rounded-lg hover:scale-105 duration-300'>
             <img src={item.image} alt={item.name}
-            className='w-full h-[200px] object-cover rounded-t-lg'
+                 className='w-full h-[200px] object-cover rounded-t-lg'
             />
             <div className='flex justify-between px-2 py-4'>
               <p className='font-bold'>{item.name}</p>
